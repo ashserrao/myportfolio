0f893186789d1261d93fbd9c95da36625174bdca
Anush Serrao <60592571+ashserrao@users.noreply.github.com>
2021-08-09 01:06:10 +0530

    my team update

--- script.js
+++ script.js
@@ -11,4 +11,9 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    //owl carousel
+    $('.carousel').owlCarousel({
+        
+    });
 });
